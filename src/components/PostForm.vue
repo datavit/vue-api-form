e9b4236a1d37b2d0e9da4ad9b7d4f83e6697547d
vit <vit.mitelman@gmail.com>
2021-05-04 19:13:36 +0300

    VUE.JS Set rest api.Form submit- ver 0.1

---
+++ src/components/PostForm.vue
@@ -0,0 +1,43 @@
+<template>
+<form @submit.prevent="submit">
+    <input tupe="text" placeholder="title" v-model="title">
+    <input type ="text" placeholder="body" v-model= "body">
+    <button tupe="submit">Create Post</button>
+    <hr>
+</form>
+</template>
+
+<style scoped>
+input {
+    display:block;
+    width:100%;
+    border:1px solid #ccc;
+    border-radius: 2px;
+    padding:10px;
+    margin-bottom:10px;
+}
+</style>
+
+<script>
+import {mapMutations} from "vuex";
+export default {
+    data() {
+        return {
+            title:"",
+            body: ""
+        };
+    },
+    methods: {
+            ...mapMutations(["createPost"]),
+        submit() {
+            this.createPost({
+                title: this.title,
+                body: this.body,
+                id: Date.now()
+            });
+            this.title = this.body = "";
+        }
+    }
+
+};
+    </script>
